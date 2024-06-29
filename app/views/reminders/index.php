047cfe29c96feb5ea4240e9d2997a0118b8596bd
GuruinderJain <gujain@algomau.ca>
2024-06-29 01:31:43 +0000

    created new model and controller for reminder

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -3,9 +3,8 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>You are at a secret page</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
-            </div>
+                <h1>Reminder</h1>
+              </div>
         </div>
     </div>
 
