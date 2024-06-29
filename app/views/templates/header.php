f5d172330e93ca66baaaa11046b26131728b28ce
GuruinderJain <gujain@algomau.ca>
2024-06-29 03:52:23 +0000

    did changes in header to add reminder in bar

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -28,17 +28,17 @@ if (!isset($_SESSION['auth'])) {
           <a class="nav-link active" aria-current="page" href="/home">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/about.php">About Me</a>
+          <a class="nav-link" href="/Reminder.php">Reminder</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            about me
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Guruinder jain</a></li>
+            <li><a class="dropdown-item" href="#">239626190</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">gujain@algomau.ca</a></li>
           </ul>
         </li>
         <li class="nav-item">
